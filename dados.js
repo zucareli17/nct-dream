997496b54e54cf11ae6a873f70387b9d1391070a
Zucareli <116381049+zucareli17@users.noreply.github.com>
2024-09-07 00:40:21 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,44 @@
+let dados = [
+    {
+        titulo: "Mark",
+        descricao: "Mark Lee é um dos membros mais queridos e versáteis do NCT Dream. Conhecido por sua rap habilidoso, dança impecável e visual marcante, ele conquistou o coração de milhões de fãs ao redor do mundo. Desde sua estreia, Mark tem demonstrado um crescimento constante como artista, participando da composição de diversas músicas do grupo e mostrando sua paixão pela música e pela performance. Sua presença carismática e talento multifacetado fazem dele um dos pilares do NCT Dream, contribuindo para o sucesso e a popularidade do grupo.",
+        link: "https://smtown-nctzens.fandom.com/wiki/MARK",
+        tags: "líder lider dançarino hyung rapper"
+    },
+    {
+        titulo: "Renjun",
+        descricao: "Renjun é um dos membros mais queridos do NCT Dream, conhecido por sua voz suave e melodiosa. Como vocalista principal do grupo, ele encanta os fãs com suas performances impecáveis e emotivas. Além de seu talento vocal, Renjun também é um dançarino habilidoso e um visualmente atraente, complementando o perfil versátil do NCT Dream. Apesar de sua idade relativamente jovem, Renjun demonstra uma maturidade e profissionalismo que o tornam um dos pilares do grupo, contribuindo para o sucesso do NCT Dream tanto na Coreia do Sul quanto internacionalmente.",
+        link: "https://smtown-nctzens.fandom.com/wiki/RENJUN",
+        tags: "vocalista chinês chines vocal"
+    },
+    {
+        titulo: "Jeno",
+        descricao: "Jeno é um dos membros mais populares do NCT Dream, conhecido por sua dança poderosa e expressiva. Como o principal dançarino do grupo, ele encanta os fãs com seus movimentos precisos e fluidos. Além de suas habilidades de dança, Jeno também é um rapper talentoso e um visual marcante, contribuindo para o sucesso do NCT Dream. Sua presença carismática e talento multifacetado fazem dele um dos pilares do grupo, e sua popularidade continua crescendo a cada dia.",
+        link: "https://smtown-nctzens.fandom.com/wiki/JENO",
+        tags: "rapper dançarino visual"
+    },
+    {
+        titulo: "Haechan",
+        descricao: "Haechan é um dos membros mais carismáticos e versáteis do NCT Dream. Conhecido por sua voz potente e expressiva, ele é um dos principais vocalistas do grupo. Além de seu talento vocal, Haechan também é um dançarino habilidoso e um performer de palco inesquecível. Sua personalidade extrovertida e energética contagia a todos ao seu redor, tornando-o um dos membros mais populares do NCT Dream. Sua versatilidade o levou a participar de diversos projetos dentro do NCT, demonstrando seu talento em diferentes áreas da música.",
+        link: "https://smtown-nctzens.fandom.com/wiki/HAECHAN",
+        tags: "vocalista vocal"
+    },
+    {
+        titulo: "Jaemin",
+        descricao: "Jaemin é um dos membros mais populares do NCT Dream, conhecido por seu visual marcante e sorriso encantador. Além de sua beleza, Jaemin é um dançarino habilidoso e um ator talentoso, tendo participado de diversos projetos de atuação. Sua presença no grupo contribui para a estética visual do NCT Dream e ele encanta os fãs com sua personalidade gentil e carismática. Apesar de ter enfrentado alguns desafios de saúde no início de sua carreira, Jaemin retornou com força e se consolidou como um dos membros mais queridos do grupo.",
+        link: "https://smtown-nctzens.fandom.com/wiki/JAEMIN",
+        tags: "rapper centro dançarino visual"
+    },
+    {
+        titulo: "Chenle",
+        descricao: "Chenle é um dos membros mais queridos do NCT Dream, conhecido por sua voz potente e melodiosa. Como um dos principais vocalistas do grupo, ele encanta os fãs com suas performances impecáveis e versáteis. Além de seu talento vocal, Chenle também é um dançarino habilidoso e um visualmente atraente, complementando o perfil versátil do NCT Dream. Originário da China, Chenle trouxe um charme único ao grupo, misturando influências culturais orientais e ocidentais em suas performances. Sua personalidade divertida e carismática o torna um dos membros mais queridos pelos fãs.",
+        link: "https://smtown-nctzens.fandom.com/wiki/CHENLE",
+        tags: "vocalista chinês chines vocal"
+    },
+    {
+        titulo: "Jisung",
+        descricao: "Jisung é o maknae (mais novo) do NCT Dream, conhecido por sua energia contagiante e talento multifacetado. Como um dos principais dançarinos do grupo, ele encanta os fãs com seus movimentos precisos e poderosos. Além de suas habilidades de dança, Jisung também é um rapper talentoso e um vocalista versátil. Sua personalidade extrovertida e carismática o tornam um dos membros mais populares do NCT Dream. Apesar de sua idade, Jisung demonstra uma maturidade e profissionalismo que o colocam no mesmo nível dos membros mais antigos do grupo.",
+        link: "https://smtown-nctzens.fandom.com/wiki/JISUNG",
+        tags: "maknae rapper dançarino"
+    }
+];
